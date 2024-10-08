3f4d1f7acc84a26174dfc62ba4c6053079139830
sergio moreno <sergio.moreno@nubiral.com>
2024-10-07 23:58:08 -0500

    MOD: se modifica main.yaml

--- infra/main.tf
+++ infra/main.tf
@@ -2,15 +2,15 @@
 
 module "glue-job" {
     source = "./modules/glue_jobs"
-    glue_job_name = var.glue_glue
+    glue_glue = var.glue_glue
 }
 
 module "database-catalog" {
   source = "./modules/glue_catalog_database"
-  database_name = var.glue_databases
+  glue_databases = var.glue_databases
 }
 
 module "table-catalog" {
   source = "./modules/glue_catalog_table" 
-  table_catalog = var.glue_tables
+  glue_tables = var.glue_tables
 }
